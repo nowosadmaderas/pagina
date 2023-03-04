5c94f9902d71b41920a4f79515ce4778a7b593b1
santi <pagmadera17@gmail.com>
2023-03-04 20:09:48 -0300

    1er pagina

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const navTogle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navx = 5;
+
+navTogle.addEventListener("click", () => {
+    navMenu.classList.toggle(".nav-menu_visible");
+});
