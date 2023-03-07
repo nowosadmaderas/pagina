4f0c065531d533b08490ce97db41fb7c1c9ca0d3
santi <pagmadera17@gmail.com>
2023-03-06 23:14:02 -0300

    avance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* menu haburguesa */
 const navTogle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 const navx = 5;
@@ -5,3 +6,19 @@ const navx = 5;
 navTogle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+/* boton leer mas */
+let hideText_btn = document.getElementById('hideText_btn')
+let hideText = document.getElementById('hideText')
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show')
+    if(hideText.classList.contains('show')){
+        hideText_btn.innerHTML = 'leer menos'
+    }
+    else{
+        hideText_btn.innerHTML = 'leer mas'
+    }
+}
